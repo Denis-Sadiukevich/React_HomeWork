1d87af1bee0d8516672c486e897f689a30f21cf0
Denis <d.sadiukevich@gmail.com>
2021-07-14 19:17:40 +0300

    HW2_1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict"
+let arr = [1, 2, 3, 4, 4, 5, 2, 9, 4];
+
+let arr1 = [];
+arr1 = arr.reduce(function unique(a, b) {
+
+   if (!arr1.includes(b)) {
+      arr1.push(b);
+   }
+   
+   return arr1;
+}, []);
+
+console.log(arr1);
