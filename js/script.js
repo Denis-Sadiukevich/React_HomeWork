a3dc8c601ca28c76073fb1f1ebacd30c5798f83c
Denis <d.sadiukevich@gmail.com>
2021-07-14 19:45:31 +0300

    repeat_js_2_1

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 let arr = [1, 2, 3, 4, 4, 5, 2, 9, 4];
 
 let arr1 = [];
-arr1 = arr.reduce(function unique(a, b) {
+arr1 = arr.reduce(function unique(c, b) {
 
    if (!arr1.includes(b)) {
       arr1.push(b);
